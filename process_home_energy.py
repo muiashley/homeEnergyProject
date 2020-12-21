c9f1b490a6ae3ab8d2f36a32cfa71d7ae537e8fb
muiashley <76234424+muiashley@users.noreply.github.com>
2020-12-21 18:43:21 -0500

    add looping through CSVs in the directory
    stack all CSVs first and then import

--- process_home_energy.py
+++ process_home_energy.py
@@ -1,50 +1,65 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec 10 21:15:53 2020
+Created on Sun Dec 20 12:05:18 2020
 
 @author: ashmui
 """
 
+import os, glob
 import pandas as pd
 import numpy as np
 from datetime import datetime
 
-storageloc='C:/Users/ashmui/Downloads/Home_Energy/'
+#loop through a directory and process all files
 
-#the energy files have 2 sections: a 2-field header and a 25 field body that starts at line 14
-df1=pd.read_csv(storageloc+'20201101_hourly_kwh.csv',skiprows=12)
+path = os.getcwd()
 
+path='C:/Users/ashmui/Downloads/Home_Energy'
+
+print(path)
+
+
+all_files = glob.glob(os.path.join(path, "*_hourly_kwh*.csv"))
+df_from_each_file = (pd.read_csv(f, sep=',',skiprows=12) for f in all_files)
+df_merged   = pd.concat(df_from_each_file, ignore_index=True)
+
+
+df_merged.to_csv( "merged.csv")
+   
 #rename the unnamed column
-df1.rename(columns = {'Unnamed: 0':'date'}, inplace = True)
+df_merged.rename(columns = {'Unnamed: 0':'date'}, inplace = True)
 
-#print info about dataframe
-df1.info(verbose=True)
+#restructure from wide to time series data
+df2=pd.melt(df_merged, id_vars=['date'], var_name='time', value_name='kwh')
 
-#solution 1
-df2=pd.melt(df1, id_vars=['date'], var_name='time', value_name='kwh')
+#################################
+#   create datetime variables   #
+#################################
 
+#datetime
 df2['datetime']=pd.to_datetime(df2['date']+' '+df2['time'],format="%m/%d/%Y %I:%M %p")
 
+#date, time
+df2['date']=pd.to_datetime(df2['date']).dt.date
+df2['time']=pd.to_datetime(df2['time']).dt.time
+
+#hour
 df2['hour']=df2['datetime'].dt.hour
 
-#df2 = df2.drop(columns=["date", "time"])
 
+#################################
+#set datetime as index and sort #
+#################################
+df2.set_index(keys='datetime',inplace=True)
 df2.sort_values('datetime', inplace=True)
 
-#df2.index=df2['datetime']
+#################################
+#       plot                    #      
+#################################
+df2['kwh'].plot()
 
-df2.plot('datetime', 'kwh', kind='bar')
 
-#access date column as series
-#date=orig.date
-
-#access hour0 as series
-#hour=orig.hour0
-
-#convert into a long table with date column, hour column, and kWh column
-#orig_t=orig.transpose()
-
-#orig_stack=orig.stack(0)
-
-#stack=pd.DataFrame(index=date,data=orig_t)
-
+#################################
+#     export                    #      
+#################################
+df2.to_csv(path+'/full_data.csv', index = True)
